ba030ca9be4a9fb4d79d7d7b7746570935ee4829
João Couto <joaocouto.jar@gmail.com>
2022-01-24 17:36:28 -0400

    fix: change mail service

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -3,8 +3,7 @@ const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
 
 const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  service: process.env.MAIL_SERVICE,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
